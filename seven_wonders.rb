5166d3373b03686bc9217221c5dc44d8ce2360ba
Elise Pham <elise.pham88@gmail.com>
2019-03-18 15:48:32 -0700

    practice interacting with LocalIQ api

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,18 +1,31 @@
 require 'httparty'
+require 'awesome_print'
+require 'dotenv'
+Dotenv.load
 
 #Starter Code:
 seven_wonders = ["Great Pyramid of Giza", "Hanging Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
 
-BASE_URL = "THE BASE URL FOR THE API REQUEST"
-key = "YOUR API TOKEN"
+BASE_URL = "https://us1.locationiq.com/v1/search.php?key=YOUR_PRIVATE_TOKEN&q=SEARCH_STRING&format=json"
+key = ENV["LOCAL_IQ"]
+
+
+# ap JSON.parse(response.body)
 
 seven_wonders_locations = seven_wonders.map do |wonder|
-  # Code to discover the locations of each wonder.
-  
+  sleep(1)
+  query_parameters = {
+    'key': key,
+    'q': wonder,
+    'format': 'json'
+  }
+  response = HTTParty.get(BASE_URL, query: query_parameters)
+  {
+    wonder => { "lat" => response[0]["lat"], "lng" => response[0]["lon"] }
+  }
 end
 
-
-
+ap seven_wonders_locations
 
 
 
